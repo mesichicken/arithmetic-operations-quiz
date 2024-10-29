212978ab53797e29b21497b00f44ad911936e480
mesi <mesicharida202@gmail.com>
2024-10-29 23:02:54 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use rand::Rng;
+
+fn main() {
+    let mut num_of_correct = 0;
+    while num_of_correct < 3 {
+        let quiz_mode = rand::thread_rng().gen_range(1..=2);
+        match quiz_mode {
+            1 => loop {
+                // 1のときは加算クイズ
+                let op1 = rand::thread_rng().gen_range(0..100);
+                let op2 = rand::thread_rng().gen_range(0..100);
+
+                println!("{} + {} = ??", op1, op2);
+                println!("?? の値を入力してください:");
+                let mut ans_input = String::new();
+                std::io::stdin().read_line(&mut ans_input).unwrap();
+                let ans_input: i32 = ans_input.trim().parse().unwrap();
+
+                dbg!(ans_input);
+                if ans_input == op1 + op2 {
+                    println!("正解!");
+                    num_of_correct += 1;
+                    break;
+                } else {
+                    println!("不正解!");
+                }
+            }
+            2 => loop { // 2のときは減算クイズ
+                let op1 = rand::thread_rng().gen_range(0..100);
+                let op2 = rand::thread_rng().gen_range(0..100);
+
+                println!("{} - {} = ??", op1, op2);
+                println!("?? の値を入力してください:");
+
+                let mut ans_input = String::new();
+                std::io::stdin().read_line(&mut ans_input).unwrap();
+                let ans_input: i32 = ans_input.trim().parse().unwrap();
+
+                dbg!(ans_input);
+                if ans_input == op1 - op2 {
+                    println!("正解!");
+                    num_of_correct += 1;
+                    break;
+                } else {
+                    println!("不正解!");
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    println!("クリア！")
+}
